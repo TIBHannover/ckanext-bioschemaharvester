24196c4464657423f605c2cabf637c106b18eb49
Bhavin Katabathuni <bhavin2897@gmail.com>
2022-02-11 12:56:02 +0100

    remove plugin python file

--- ckanext/bioschemaharvester/plugin.py
+++ ckanext/bioschemaharvester/plugin.py
@@ -2,14 +2,11 @@ import logging
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from ckanext.bioschemaharvester.harvester.bioschemascrap import BioSchemaMUController
 
-from flask import Blueprint
 
 
 class BioschemaharvesterPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.ITemplateHelpers)
 
     """
@@ -22,20 +19,8 @@ class BioschemaharvesterPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('public/statics', 'ckanext-bioschemaharvester')
 
-    # plugin Blueprint
-    def get_blueprint(self):
-        blueprint = Blueprint(self.name, self.__module__)
-        blueprint.template_folder = u'templates'
-        blueprint.add_url_rule(
-            u'/bioschemaharvester/',
-            u'_get_dataseturl',
-            BioSchemaMUController._get_dataseturl,
-            methods=['POST']
-        )
-        return blueprint
-
     # ITemplateHelpers
 
     def get_helpers(self):
-       return {'_get_dataseturl': BioSchemaMUController._get_dataseturl}
+       pass
 

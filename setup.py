b3ee371c5d4a374046d999851f654d4a01ae7126
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-04-22 16:42:27 +0200

    test nmrxiv plugin with bioschema

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    version='0.1.0',
 
     description='''This extension is to provide scrapper or harvesting metadata from home.org where Schema.org datasets use sitemaps to locate datasets. ''',
     long_description=long_description,
@@ -45,7 +45,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.x',
     ],
 
 
